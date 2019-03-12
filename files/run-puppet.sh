788ea7ae5ef4cbd000ace041ec8ff479d2e92f33
Palmya95 <butsav@gmail.com>
2019-03-12 11:45:23 +0100

    Update run-puppet.sh

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
 /opt/puppetlabs/bin/puppet apply manifests/#!/bin/bash
-cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/bin/puppet apply manifests/
